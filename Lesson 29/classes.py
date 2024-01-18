89f0dddd2850768719b1a73bc1eeadcb18a60d95
Artem <mrkozlovartem@gmail.com>
2024-01-18 08:40:33 +0300

    Урок 29

---
+++ Lesson 29/classes.py
@@ -0,0 +1,20 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+class Human():
+    def __init__(self, year, month, day):
+        self.birthday = datetime.datetime(year, month, day)
+
+    def get_age(self):
+        return relativedelta(datetime.datetime.now(), self.birthday).years
+
+
+if __name__ == '__main__':
+    humans = []
+    humans.append(Human(2020, 1, 10))
+    humans.append(Human(2013, 5, 10))
+    humans.append(Human(2010, 1, 20))
+    humans.append(Human(1970, 3, 30))
+
+    for human in humans:
+        print(human.get_age())
